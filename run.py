b5d7ccf83949e8711eb0221708a837d4dfb58c3e
lluuiq <45070125+lluuiq@users.noreply.github.com>
2020-04-05 14:34:48 +0800

    lluuiq
    bate

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 # @File : run.py.py
 # @project : Hexo-Management
 # @Time : 2020/4/3 12:13
-
+import shutil
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -22,17 +22,26 @@ class Window():
         self.window = uic.loadUi("ui/window.ui")
         # 创建settings，用于保存和初始化信息
         self.settings = QSettings("config.ini", QSettings.IniFormat)
-        ########## 初始化
+
+        #################### 初始化 ####################
         self.blog_root = "None"
-        self.username = "None"
-        self.password = "None"
+        self.post_path = "None"
+        self.draft_path = "None"
+        # self.username = "None"
+        # self.password = "None"
         self.git_path = "None"
         self.branch = "None"
         self.initInfo()
-        # 创建线程用于存储调试时的线程
+
+        '''创建hexo server的线程与锁'''
         self.thread_server = QThread()
         self.mutex = QMutex()
-        # 更改os目录
+
+        '''更新文章目录'''
+        self.updataTable(self.post_path, self.window.tableWidget_post)
+        self.updataTable(self.draft_path, self.window.tableWidget_draft)
+
+        ''' 更改os目录 '''
         os.chdir(self.blog_root)
 
         '''关闭登录功能'''
@@ -40,13 +49,13 @@ class Window():
         self.window.lineEdit_password.setDisabled(True)
         self.window.pushButton_login.setDisabled(True)
 
-        ########## 绑定事件
+        #################### 绑定事件 ####################
         self.window.pushButton_clear.clicked.connect(self.clear)
-        # 设置界面
-        self.window.toolButton_blog_root.clicked.connect(self.getBlogRoot)  # 获取博客根目录
+        ''' 设置界面 '''
+        self.window.pushButton_blog_root.clicked.connect(self.getBlogRoot)  # 通过按钮获取博客根目录
         # self.window.pushButton_login.clicked.connect(self.logIn) # 登录用户（用不到）
 
-        # 博客操作界面
+        ''' 博客操作界面 '''
         self.window.pushButton_back_up.clicked.connect(self.backUp)  # 备份
         self.window.pushButton_open_blog.clicked.connect(self.openBlog)  # 打开博客根目录
         self.window.pushButton_server.clicked.connect(self.server)  # hexo s
@@ -57,17 +66,30 @@ class Window():
         self.window.pushButton_deploy.clicked.connect(self.deploy)  # hexo d
         self.window.pushButton_c_g_d.clicked.connect(self.cleanGenerateDeploy)  # clean + g +d
 
-        # 文章管理界面
-        self.window.pushButton_new_post.clicked.connect(self.newPost)
+        '''文章管理界面'''
+        self.window.pushButton_new_post.clicked.connect(self.newPost)  # 新建文章
+        self.window.pushButton_modify_post.clicked.connect(self.modifyPost)  # 编辑文章
+        self.window.pushButton_del_post.clicked.connect(self.delPost)  # 删除文章
+        self.window.pushButton_move_post.clicked.connect(self.movePost)  # 文章移动到草稿箱
+
+        self.window.pushButton_new_draft.clicked.connect(self.newDraft)  # 新建草稿
+        self.window.pushButton_modify_draft.clicked.connect(self.modifyDraft)  # 编辑草稿
+        self.window.pushButton_del_draft.clicked.connect(self.delDraft)  # 删除草稿
+        self.window.pushButton_deploy_draft.clicked.connect(self.moveDraft)  # 草稿移动到文章列表
 
     # 初始化数据函数
     def initInfo(self):
+        # 初始化路径
         self.blog_root = self.settings.value("blog_root")
-        # self.username = self.settings.value("username")
-        # self.password = self.settings.value("password")
+        self.post_path = self.blog_root + "/source/_posts"
+        self.draft_path = self.blog_root + "/source/_drafts"
+
         self.git_path = self.settings.value("git_path")
         self.branch = self.settings.value("branch")
         self.message = self.settings.value("message")
+        # self.username = self.settings.value("username")
+        # self.password = self.settings.value("password")
+
         self.window.label_blog_root.setText(self.blog_root)
         # self.window.label_username.setText(self.username)
         # self.window.label_password.setText(self.password)
@@ -76,21 +98,31 @@ class Window():
         self.window.lineEdit_message.setText(self.message)
 
         os.chdir(self.blog_root)
-        self.window.plainTextEdit.appendPlainText("=" * 100 + "\n" + "当前博客根目录为: " + os.getcwd() + "\n" + "=" * 100)
+        self.window.plainTextEdit.appendPlainText(
+            "=" * 100 + "\n" + "当前博客根目录为: " + os.getcwd() + "\n" + "=" * 100
+        )
 
     # 获取博客根目录
     def getBlogRoot(self):
-        # 获取路径
-        self.blog_root = QFileDialog.getExistingDirectory(self.window,
-                                                          "选取文件夹", "D:/")  # 起始路径
-        # 更改标签路径
-        self.window.label_blog_root.setText(self.blog_root)
-        # 更改os目录
-        os.chdir(self.blog_root)
-        # 保存路径
-        self.settings.setValue("blog_root", self.window.label_blog_root.text())
-        # 输出路径
-        self.window.plainTextEdit.appendPlainText("更改博客根目录为: " + os.getcwd())
+        def threadRun(args):
+            # # 获取路径
+            blog_root = QFileDialog.getExistingDirectory(self.window,
+                                                         "选取博客根目录", "D:/")  # 起始路径
+            if blog_root.strip():
+                if blog_root != self.blog_root:
+                    self.blog_root = blog_root
+                    # 更改标签路径
+                    self.window.label_blog_root.setText(self.blog_root)
+                    # 更改os目录
+                    os.chdir(self.blog_root)
+                    # 保存路径
+                    self.settings.setValue("blog_root", self.window.label_blog_root.text())
+                    # 输出路径
+                    self.window.plainTextEdit.appendPlainText("更改博客根目录为: " + os.getcwd())
+
+        thread_getBlogRoot = QThread()
+        thread_getBlogRoot.run = types.MethodType(threadRun, thread_getBlogRoot)
+        thread_getBlogRoot.start()
 
     # 登录用户
     '''
@@ -147,12 +179,13 @@ class Window():
 
         # 创建线程
         thread_backUp = QThread()
-        # 若没有.git文件夹，则初始化仓库
+        # 若没有.git文件夹，则初始化仓库 执行init函数
         if not os.path.isdir(".git"):
-            self.window.plainTextEdit.appendPlainText("检测到本地没有.git目录，初始化仓库，请等待完成")
+            self.window.plainTextEdit.appendPlainText("检测到本地没有.git目录，初始化仓库，请等待完成后再点击备份")
             thread_backUp.run = types.MethodType(init, thread_backUp)
             thread_backUp.start()
         else:
+            # 存在.git ， 执行execute函数，开始备份
             thread_backUp.run = types.MethodType(execute, thread_backUp)
             thread_backUp.start()
 
@@ -191,7 +224,8 @@ class Window():
             self.thread_server.run = types.MethodType(threadRun, self.thread_server)
             # 输出信息
             self.window.plainTextEdit.appendPlainText(
-                '=' * 100 + '\n' + '调试模式启动，若要执行其他hexo指令请先关闭调试模式再执行，关闭工具会自动关闭调试。\n' + 'tip:调试模式下可以进行主题相关的修改')
+                '=' * 100 + '\n' + '调试模式启动，若要执行其他hexo指令请先关闭调试模式再执行，关闭工具会自动关闭调试。\n ' \
+                + 'tip:调试模式下可以进行主题相关的修改')
             ### 启动线程
             self.thread_server.start()
 
@@ -227,7 +261,7 @@ class Window():
     因为clean、generate、deploy的指令代码类似，故将执行部分封装为一个函数
     '''
 
-    def hexoCmd(self, cmd, thread):
+    def Cmd(self, cmd, thread):
         def threadRun(args):
             self.mutex.lock()
             self.window.progressBar.setMaximum(0)
@@ -248,28 +282,158 @@ class Window():
     def clean(self):
         # 创建一个线程
         thread_clean = QThread()
-        # 通过hexoCmd函数来执行指令
-        self.hexoCmd("hexo clean", thread_clean)
+        # 通过Cmd函数来执行指令
+        self.Cmd("hexo clean", thread_clean)
 
     def generate(self):
         thread_generate = QThread()
-        self.hexoCmd("hexo g", thread_generate)
+        self.Cmd("hexo g", thread_generate)
 
     def deploy(self):
         thread_deploy = QThread()
-        self.hexoCmd("hexo d", thread_deploy)
+        self.Cmd("hexo d", thread_deploy)
 
     def cleanGenerateDeploy(self):
         thread_all = QThread()
-        self.hexoCmd("hexo clean && hexo g && hexo d", thread_all)
+        self.Cmd("hexo clean && hexo g && hexo d", thread_all)
 
+    # 用于获取当前选中的文章
+    def getPosts(self, tableWidget):
+        items = tableWidget.selectedItems()[::3]
+        titles = []
+        for title in items:
+            titles.append(title.text())
+        return titles
+
+    # 创建新文章
     def newPost(self):
-        title = self.window.lineEdit_post_title.text()
+        def threadRun(args):
+            # 获取标题
+            title = self.window.lineEdit_post_title.text()
+
+            # 执行hexo指令，创建文章
+            thread = QThread()
+            self.Cmd("hexo new " + '\"' + title + '\"', thread)
+            # 等待线程结束，更新table
+            thread.wait()
+            self.updataTable(self.post_path, self.window.tableWidget_post)
+
         thread_newPost = QThread()
-        self.hexoCmd("hexo new " + '\"' + title + '\"', thread_newPost)
+        thread_newPost.run = types.MethodType(threadRun, thread_newPost)
+        thread_newPost.start()
 
+    # 编辑选中文章
     def modifyPost(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_post)
+            for title in titles:
+                os.startfile(self.post_path + '/' + title + '.md')
 
+        thread_modifyPost = QThread()
+        thread_modifyPost.run = types.MethodType(threadRun, thread_modifyPost)
+        thread_modifyPost.start()
+
+    # 删除文章 (没有到回收站)
+    def delPost(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_post)
+            for title in titles:
+                os.remove(self.post_path + '/' + title + '.md')
+            self.updataTable(self.post_path, self.window.tableWidget_post)
+
+        thread_delPost = QThread()
+        thread_delPost.run = types.MethodType(threadRun, thread_delPost)
+        thread_delPost.start()
+
+    # 文章移动到草稿箱
+    def movePost(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_post)
+            for title in titles:
+                shutil.move(self.post_path + '/' + title + '.md', self.draft_path + '/' + title + '.md')
+            self.updataTable(self.post_path, self.window.tableWidget_post)
+            self.updataTable(self.draft_path, self.window.tableWidget_draft)
+
+        thread_movePost = QThread()
+        thread_movePost.run = types.MethodType(threadRun, thread_movePost)
+        thread_movePost.start()
+
+    # 创建新草稿
+    def newDraft(self):
+        def threadRun(args):
+            # 获取标题
+            title = self.window.lineEdit_draft_title.text()
+            thread = QThread()
+            self.Cmd("hexo new draft " + '\"' + title + '\"', thread)
+            thread.wait()
+            self.updataTable(self.draft_path, self.window.tableWidget_draft)
+
+        thread_newDraft = QThread()
+        thread_newDraft.run = types.MethodType(threadRun, thread_newDraft)
+        thread_newDraft.start()
+
+    # 编辑选中草稿
+    def modifyDraft(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_draft)
+            for title in titles:
+                os.startfile(self.draft_path + '/' + title + '.md')
+
+        thread_modifyDraft = QThread()
+        thread_modifyDraft.run = types.MethodType(threadRun, thread_modifyDraft)
+        thread_modifyDraft.start()
+
+    # 删除选中草稿
+    def delDraft(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_draft)
+            for title in titles:
+                os.remove(self.draft_path + '/' + title + '.md')
+            self.updataTable(self.draft_path, self.window.tableWidget_draft)
+
+        thread_delDraft = QThread()
+        thread_delDraft.run = types.MethodType(threadRun, thread_delDraft)
+        thread_delDraft.start()
+
+    # 草稿箱移动到文章目录
+    def moveDraft(self):
+        def threadRun(args):
+            titles = self.getPosts(self.window.tableWidget_draft)
+            for title in titles:
+                shutil.move(self.draft_path + '/' + title + '.md', self.post_path + '/' + title + '.md')
+            self.updataTable(self.post_path, self.window.tableWidget_post)
+            self.updataTable(self.draft_path, self.window.tableWidget_draft)
+
+        thread_moveDraft = QThread()
+        thread_moveDraft.run = types.MethodType(threadRun, thread_moveDraft)
+        thread_moveDraft.start()
+
+    # 更新文章tabel的函数，path参数为： 根目录/source/_posts或_drafts ，tableWidget传入对应表格
+    def updataTable(self, path, tableWidget):
+        # 清除表中的内容
+        tableWidget.clearContents()
+        # 将表格变为0行
+        tableWidget.setRowCount(0)
+
+        # 获取全部文件
+        post_list = os.listdir(path)
+        # 遍历文件，寻找markdown文件
+        for post in post_list:
+            # os.path.splitext()用于分离文件名与扩展名
+            split_text = os.path.splitext(post)
+            if split_text[1] == '.md':
+                title = split_text[0]
+                # 修改日期格式，时间戳转日期
+                mtime = os.stat(path + '/' + post).st_mtime
+                ctime = os.stat(path + '/' + post).st_ctime
+                modify_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(mtime))
+                build_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ctime))
+                row = tableWidget.rowCount()
+                tableWidget.insertRow(row)
+                # 插入到tabelWidget中
+                tableWidget.setItem(row, 0, QTableWidgetItem(title))
+                tableWidget.setItem(row, 1, QTableWidgetItem(build_time))
+                tableWidget.setItem(row, 2, QTableWidgetItem(modify_time))
 
     # 清除输出信息
     def clear(self):
@@ -282,5 +446,4 @@ if __name__ == '__main__':
 
     stats = Window()
     stats.window.show()
-    stats.window.tableWidget_post.
     sys.exit(app.exec_())
